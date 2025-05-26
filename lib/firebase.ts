9920cbc36e56db3abbb786f61c366ba06e8409d7
triton100 <sphamandla@bitstech.co.za>
2025-05-26 11:37:19 +0200

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getDatabase } from "firebase/database"
 import { getFirestore } from "firebase/firestore"
-
+import { getStorage } from "firebase/storage"
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -19,3 +19,4 @@ const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const rtdb = getDatabase(app) 
+export const storage = getStorage(app)
